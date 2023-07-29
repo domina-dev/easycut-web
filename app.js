6bfc1f4aa190c922238fc5d1204c155bb1ff548c
Vitor Macedo <evitorfiler@gmail.com>
2023-07-29 20:30:22 -0300

    port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/',function(req,res){
     res.sendFile('index.html', {root: 'dist/vex/'}
   );
 })
-app.listen(port, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 4200, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
