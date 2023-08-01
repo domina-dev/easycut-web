78773e36396f0ac702315c2817cc9e52769f7a64
Jotapey33 <jpsouzafiuza@gmail.com>
2023-07-31 21:54:43 -0300

    adicionando tabela na tab de agendamento

--- src/app/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component.ts
+++ src/app/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'exibicao-agendamentos',
   templateUrl: './exibicao-agendamentos.component.html',
-  styleUrls: ['./exibicao-agendamentos.component.scss']
+  styleUrls: ['./exibicao-agendamentos.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class ExibicaoAgendamentosComponent implements OnInit {
+export class ExibicaoAgendamentosComponent implements AfterViewInit {
+  displayedColumns: string[] = ['cliente', 'servico', 'tempo', 'valor', 'data', 'responsavel', 'botao'];
+  dataSource = new MatTableDataSource<Agendamentos>(CLIENTE_DATA);
 
-  constructor() { }
+  panelColor = new FormControl('pendente');
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() {
+
+   }
+
+   ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
-}
+  }
+
+  export interface Agendamentos {
+    cliente: string;
+    servico: string;
+    tempo: string;
+    valor: string;
+    data: string;
+    responsavel: string;
+  }
+    const CLIENTE_DATA: Agendamentos[] = [
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+      {cliente: 'João Pedro Fiuza', servico: 'Cabelo + barba', tempo: '60 min', valor: 'R$ 80,00', data: '13/03/2023', responsavel: 'Vito Nunes',},
+    ];
