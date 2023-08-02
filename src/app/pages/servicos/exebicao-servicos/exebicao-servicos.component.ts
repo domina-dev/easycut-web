210ec6f19970176b2885528bef7a87064a3eec6f
Jonatas G Nahorny <jonatasnahorny@gmail.com>
2023-08-02 11:24:14 -0300

    criação de tabela na aba serviços/exibição-servicos

--- src/app/pages/servicos/exebicao-servicos/exebicao-servicos.component.ts
+++ src/app/pages/servicos/exebicao-servicos/exebicao-servicos.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'exebicao-servicos',
   templateUrl: './exebicao-servicos.component.html',
   styleUrls: ['./exebicao-servicos.component.scss']
 })
-export class ExebicaoServicosComponent implements OnInit {
+export class ExebicaoServicosComponent implements AfterViewInit {
+  displayedColumns: string[] = ['aplicacao', 'servico', 'descricao', 'tempo', 'preco', 'icone'];
+  dataSource = new MatTableDataSource<ListaProdutos>(ELEMENT_DATA);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
+export interface ListaProdutos {
+  aplicacao: string;
+  servico: string;
+  descricao: string;
+  tempo: string;
+  preco: string;
+  icone: string;
+}
+
+const ELEMENT_DATA: ListaProdutos[] = [
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  {aplicacao:'Cabelo' ,servico: 'Corte simples', descricao: 'Corte de cabelo masculino simples', tempo: '30 min-50min', preco: 'R$ 30,00', icone: ''},
+  
+];
+
+/*import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface servicos {
+  servico: string;
+  produto: string;
+  descricao: string;
+  quantidade: string;
+  valor: string;
+  
+}
+
+let ELEMENT_DATA: servicos[] = [
+  // {servico: "", produto: "", quantidade: "", valor: ""},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+  {servico: "corte", produto: "pomada modeladora alpha", descricao:"pomada modeladora efeito teia", quantidade: "2 unidades", valor: "50,00"},
+
+];
+
+@Component({
+  selector: 'exebicao-servicos',
+  templateUrl: './exebicao-servicos.component.html',
+  styleUrls: ['./exebicao-servicos.component.scss']
+})
+export class ExebicaoServicosComponent implements OnInit, AfterViewInit  {
+  displayedColumns: string[] = ['servico', 'produto', 'descricao', 'quantidade','valor'];
+  dataSource = new MatTableDataSource<servicos>(ELEMENT_DATA);
+  
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+
 }
+*/
--- src/app/pages/servicos/exebicao-servicos/exebicao-servicos.module.ts
+++ src/app/pages/servicos/exebicao-servicos/exebicao-servicos.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ExebicaoServicosRoutingModule } from './exebicao-servicos-routing.module';
 import { ExebicaoServicosComponent } from './exebicao-servicos.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +16,10 @@ import { ExebicaoServicosComponent } from './exebicao-servicos.component';
   ],
   imports: [
     CommonModule,
-    ExebicaoServicosRoutingModule
+    ExebicaoServicosRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule
   ],
   exports: [ExebicaoServicosComponent]
 })
