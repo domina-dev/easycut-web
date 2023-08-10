8a741dae51bbb83c240c4a25b3e8c4fb34156a7d
Victorsn07 <vitorsntossouza9866@gmail.com>
2023-08-10 18:22:08 -0300

    components do modal servico

---
+++ src/app/modais/servico/cadastrar-editar-servico/cadastrar-editar-servico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vex-cadastrar-editar-servico',
+  templateUrl: './cadastrar-editar-servico.component.html',
+  styleUrls: ['./cadastrar-editar-servico.component.scss']
+})
+export class CadastrarEditarServicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
