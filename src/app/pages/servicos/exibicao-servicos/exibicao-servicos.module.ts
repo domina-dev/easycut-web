f8ca9abe1c365e7f558f8ff8d7b77af1500edefd
Victorsn07 <vitorsntossouza9866@gmail.com>
2023-08-10 14:32:01 -0300

    fiz a correção dos conflitos apresentados na solicitação de pull request

--- src/app/pages/servicos/exibicao-servicos/exibicao-servicos.module.ts
+++ src/app/pages/servicos/exibicao-servicos/exibicao-servicos.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 import { ExibicaoServicosRoutingModule } from './exibicao-servicos-routing.module';
 import { ExibicaoServicosComponent } from './exibicao-servicos.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
