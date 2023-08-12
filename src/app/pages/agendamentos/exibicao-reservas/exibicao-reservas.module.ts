fd8c2830a54136a9c2ee23609a0dd30c0c017f78
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-08-11 21:05:17 -0300

    colocando comando npm run format

--- src/app/pages/agendamentos/exibicao-reservas/exibicao-reservas.module.ts
+++ src/app/pages/agendamentos/exibicao-reservas/exibicao-reservas.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ExibicaoReservasRoutingModule } from './exibicao-reservas-routing.module';
-import { ExibicaoReservasComponent } from './exibicao-reservas.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatSelectModule } from '@angular/material/select';
+import { ExibicaoReservasRoutingModule } from "./exibicao-reservas-routing.module";
+import { ExibicaoReservasComponent } from "./exibicao-reservas.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
-  declarations: [
-    ExibicaoReservasComponent
-  ],
+  declarations: [ExibicaoReservasComponent],
   imports: [
     CommonModule,
     ExibicaoReservasRoutingModule,
     MatTableModule,
-    MatSelectModule
+    MatSelectModule,
   ],
-  exports:[
-  ExibicaoReservasComponent,
-  ]
+  exports: [ExibicaoReservasComponent],
 })
 export class ExibicaoReservasModule {}
