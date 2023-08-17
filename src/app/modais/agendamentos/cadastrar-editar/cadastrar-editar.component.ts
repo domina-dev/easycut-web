ee5ca9227c25f8f6c5153b8ad4eb2dd4963c7472
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-17 19:58:19 -0300

    mudanças em login, no filtro do gean e termino da tarefa com correção do bug que estava afetando os forms da galera

--- src/app/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
+++ src/app/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Agendamentos } from 'src/app/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component';
 
 @Component({
   selector: 'vex-cadastrar-editar',
   templateUrl: './cadastrar-editar.component.html',
   styleUrls: ['./cadastrar-editar.component.scss']
 })
-export class CadastrarEditarComponent implements OnInit  {
+export class CadastrarEditarComponent {
   public clientForm: FormGroup;
+
+  public userData?: Agendamentos;
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly dialogRef: MatDialogRef<any>,
-    ) {
+  ) {
     this.clientForm = fb.group({
-      nome: [''],
-      cpf: [''],
-      sexo: [''],
-      dataNascimento: [''],
-      pcd: [''],
-      email: [''],
-      telefone: [''],
-      rua: [''],
-      n: [''],
+      cliente: [''],
+      servico: [''],
+      tempo: [''],
+      valor: [''],
+      data: [''],
+      responsavel: [''],
+      status: [''],
     })
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
   }
 
-
+  public addUser() {
+    this.userData = this.clientForm.value;
+    this.dialogRef.close(this.userData);
+  }
 
 }
