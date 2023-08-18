91b144837519e778b0b132690c889c00eb60564d
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-18 15:27:02 -0300

    criando e comentando método para chamar http

---
+++ src/app/services/planos/planos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanosService {
+
+  // private readonly API = '';
+
+  // constructor(private HttpClient: HttpClient) { }
+  // // declarar a dependencia HttpClient no app module
+
+  // list() {
+
+  //   return this.HttpClient.get/*<obterPlanos[]>*/(this.API)
+  //     .pipe(
+  //       take(1)
+  //     )
+  //   // obterPlanos deve estar linkado a estrutura TS do onde vai aparecer a lista de planos com suas devidas
+  //   // referencias, e no nosso caso as devidas observables
+  // }
+}
+
