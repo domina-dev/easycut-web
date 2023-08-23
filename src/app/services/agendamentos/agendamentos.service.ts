ae29eeb96306ba92447d01bf385c3198b1d38eb9
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-08-23 18:22:49 -0300

    ajustando service de agendamentos

--- src/app/services/agendamentos/agendamentos.service.ts
+++ src/app/services/agendamentos/agendamentos.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Agendamentos } from 'src/app/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendamentosService {
-  private readonly API = '';
+  private readonly API = 'environment.url_api';
 
   constructor(private http: HttpClient) {}
 
-  obterAgendamentos() {
-    return this.http.get(this.API);
+  obterAgendamentos(): Observable<Agendamentos[]> {
+    return this.http.get<Agendamentos[]>(this.API);
   }
 }
