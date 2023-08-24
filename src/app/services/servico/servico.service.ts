af5e4ad440d9d0860c80847ec731a02e9b9d2e34
Jonatas G Nahorny <jonatasnahorny@gmail.com>
2023-08-24 11:32:24 -0300

    ajustando service de servico

--- src/app/services/servico/servico.service.ts
+++ src/app/services/servico/servico.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicoService {
+  private url = environment.url_api;
 
-  // constructor(private HttpClient: HttpClient) { 
+  constructor(
+    private HttpClient: HttpClient
+    ){}
 
-  // }
-
-  // nomeDoMetodo(){
-  //   return this.HttpClient.get<Usuario[]>(this.url);
-  // }
+  obterServico(): Observable<Servico[]> {
+    return this.HttpClient.get<Servico[]>(this.url);
+  }
+  
 }
+
 
