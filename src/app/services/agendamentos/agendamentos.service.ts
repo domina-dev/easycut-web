9e89e09e9d57be108d931a047ba25ea4fc77447b
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-08-24 17:58:52 -0300

    fazendo import de environment

--- src/app/services/agendamentos/agendamentos.service.ts
+++ src/app/services/agendamentos/agendamentos.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Agendamentos } from 'src/app/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendamentosService {
-  private readonly API = 'environment.url_api';
+  private readonly API = environment.url_api;
 
   constructor(private http: HttpClient) {}
 
