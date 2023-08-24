f42ea19ddacaa0cd7c48ee982043b6d92b15a2ef
brunodeveloperbest <129685774+brunodeveloperbest@users.noreply.github.com>
2023-08-24 20:30:27 -0300

    subindo tarefa

--- src/app/services/produtos/produtos.service.ts
+++ src/app/services/produtos/produtos.service.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from 'src/app/pages/servicos/exibicao-produtos/exibicao-produtos.component';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
-  private readonly API = '';
+  private url = environment.url_api;
 
-  constructor(private HttpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  obterProdutos() {
-    return this.HttpClient.get(this.API);
+  obterProduto(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this.url);
   }
 }
+ 
