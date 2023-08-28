6950a4b14fb12fe3b9d58e5a293558bec3c75c76
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-27 21:49:55 -0300

    criando metodo de recuperação de senha no service

--- src/app/services/estabelecimento/estabelecimento.service.ts
+++ src/app/services/estabelecimento/estabelecimento.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstabelecimentoService {
-  private readonly API = '';
+  private readonly API = 'environment.url_api';
 
 constructor(private http: HttpClient) {}
 
   obterEstabelecimento() {
     return this.http.get(this.API)
   }
+
+  recuperacaoSenha(email: string): Observable<any> {
+    return this.http.get<any>(`${this.API}/estabelecimento/recuperacao`, {
+    params: { email: email }, observe: "response",
+    });
+    }
+
 }
 
 
