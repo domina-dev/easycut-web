ca6069c40265bafa9a4b20ffad1db45b82ec7b75
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-27 21:19:55 -0300

    ajuste do service reservas

--- src/app/services/reservas/reservas.service.ts
+++ src/app/services/reservas/reservas.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reservas } from 'src/app/model/reservas';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ReservasService {
-    /*
-  getReservas(): Observable<Reservas[]> {
-    return this.http.get<Reservas[]>(this.apiUrl);
+
+  private readonly API = 'environment.url_api';
+
+  constructor(private http: HttpClient) { }
+
+  obterReservas(): Observable<Reservas[]> {
+    return this.http.get<Reservas[]>(this.API);
   }
 
 
-   */
 }
