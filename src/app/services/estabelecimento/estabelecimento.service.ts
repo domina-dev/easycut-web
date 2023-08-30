44e7072b410726e12f9b334dd145d74e52efdaf7
Jonatas G Nahorny <jonatasnahorny@gmail.com>
2023-08-29 21:40:41 -0300

    Feat: Declarando método recuperarSenha

--- src/app/services/estabelecimento/estabelecimento.service.ts
+++ src/app/services/estabelecimento/estabelecimento.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ constructor(private http: HttpClient) {}
 
   obterEstabelecimento() {
     return this.http.get(this.API)
+  }
+  recuperarSenha(): Observable<any> {
+    // Implemente a lógica para recuperar a senha
+    return this.http.get(this.API);
   }
 }
 
