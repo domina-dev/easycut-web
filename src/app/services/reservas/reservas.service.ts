3f75060c44c5aecd7fa89b7b18c903c9b9bb2d71
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-31 19:03:10 -0300

    tirando aspas do enviroment da API

--- src/app/services/reservas/reservas.service.ts
+++ src/app/services/reservas/reservas.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Reserva } from 'src/app/model/reservas';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservasService {
 
-  private readonly API = 'environment.url_api';
+  private readonly API = environment.url_api;
 
   constructor(private http: HttpClient) { }
 
