015f5fca0db7da41139219f2d5d158b75497b01f
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-08-31 18:56:16 -0300

    adicionando metodo de recuperação de senha dentro do service de estabelecimento

--- src/app/services/estabelecimento/estabelecimento.service.ts
+++ src/app/services/estabelecimento/estabelecimento.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstabelecimentoService {
-  private readonly API = 'environment.url_api';
+  private readonly API = environment.url_api;
 
 constructor(private http: HttpClient) {}
 
