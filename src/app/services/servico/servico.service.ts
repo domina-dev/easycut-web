fda942ccd85975fe9d544e87d4649b8b8234c67e
brunodeveloperbest <129685774+brunodeveloperbest@users.noreply.github.com>
2023-08-31 20:47:04 -0300

    conclusão de tarefa PDV040 - Listagem de Serviços via backend, acertado itens de tabela e configuração da requisição http

--- src/app/services/servico/servico.service.ts
+++ src/app/services/servico/servico.service.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Servico } from 'src/app/pages/servicos/exibicao-servicos/exibicao-servicos.component';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicoService {
+  private readonly API = environment.url_api;
 
-  // constructor(private HttpClient: HttpClient) { 
+  constructor(private http: HttpClient) { 
 
-  // }
-
-  // nomeDoMetodo(){
-  //   return this.HttpClient.get<Usuario[]>(this.url);
-  // }
+  }
+  obterServicos():Observable<Servico[]>{
+    return this.http.get<Servico[]>(`${this.API}/servicos/todos?estabelecimento_ID=4`);
+  }
 }
 
