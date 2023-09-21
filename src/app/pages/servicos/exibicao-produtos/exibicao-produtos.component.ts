18434bc597ed39a5c0493c4e0d38cd83e0db4dc5
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-09-20 23:29:17 -0300

    adicionando snackbar para mensagens de sucesso e erro

--- src/app/pages/servicos/exibicao-produtos/exibicao-produtos.component.ts
+++ src/app/pages/servicos/exibicao-produtos/exibicao-produtos.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { CadastrarProdutoComponent } from 'src/app/modais/produto/cadastrar-produto/cadastrar-produto.component';
 import { Produto } from '../../../model/produto'
 import { ProdutoService } from '../../../services/produtos/produtos.service'
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'vex-exibicao-produtos',
@@ -12,6 +13,7 @@ import { ProdutoService } from '../../../services/produtos/produtos.service'
     styleUrls: ['./exibicao-produtos.component.scss']
 })
 export class ExibicaoProdutosComponent implements AfterViewInit, OnInit {
+
     displayedColumns: string[] = [
         'nomeProduto',
         'descricao',
@@ -19,49 +21,66 @@ export class ExibicaoProdutosComponent implements AfterViewInit, OnInit {
         'preco',
         'icone'
     ];
-    
-    ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
-    }
-    
-    ngOnInit(): void {
-        this.listarProdutos();
-    }
+
+    listaProduto: Produto[] = []
 
     dataSource = new MatTableDataSource<Produto>();
 
     verLista: boolean = true;
     verGrade: boolean = false;
-   
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    
-    listaProduto: Produto[] = []
 
-    constructor(public dialog: MatDialog, private produtoService: ProdutoService) {}
-    
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+    }
+
+    ngOnInit(): void {
+        this.listarProdutos();
+    }
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    constructor(public dialog: MatDialog, private produtoService: ProdutoService,
+        private snackbar: MatSnackBar) { }
+
     listarProdutos() {
         this.produtoService.obterProdutos().subscribe(response => {
             this.listaProduto = response;
             this.dataSource = new MatTableDataSource<Produto>(this.listaProduto);
             this.dataSource.paginator = this.paginator;
         },
-        (error) => {console.log(error)});
+            (error) => { console.log(error) });
     }
 
     deletarProduto() {
         this.produtoService.deletaProduto().subscribe(response => {
-            console.log("Deletado com sucesso")
             this.listaProduto = response;
             this.dataSource = new MatTableDataSource<Produto>(this.listaProduto);
             this.dataSource.paginator = this.paginator;
-      }, (error) => {console.log(error, "Falha ao deletar produto")});
+            this.snackbar.open(
+                "Produto deletado com sucesso",
+                "Fechar",
+                {
+                    duration: 10000
+                }
+            );
+        }, (error) => {
+            console.log(error)
+            this.snackbar.open(
+                "Produto não deletado",
+                "Tenta novamente",
+                {
+                    duration: 10000
+                }
+            );
+
+        });
 
     }
-    
+
     openAdd() {
         this.dialog.open(CadastrarProdutoComponent);
     }
-    
+
     visualizar() {
         this.verLista = !this.verLista;
         this.verGrade = !this.verGrade;
