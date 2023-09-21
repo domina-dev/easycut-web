b90e0f6a6ed7c4ca62244d4983f036c586108988
Victor <vitorsntossouza9866@gmail.com>
2023-09-21 20:01:13 -0300

    componetizacao das tabelas do materiaL

--- src/app/lib/componentes/izi-table/izi-table.component.ts
+++ src/app/lib/componentes/izi-table/izi-table.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Agendamento } from 'src/app/model/agendamento';
+import { Cliente } from 'src/app/model/cliente';
+import { Estabelecimento } from 'src/app/model/estabelecimento';
+import { Produto } from 'src/app/model/produto';
+import { Reserva } from 'src/app/model/reservas';
+import { Servico } from 'src/app/pages/servicos/exibicao-servicos/exibicao-servicos.component';
 
 @Component({
   selector: 'vex-izi-table',
@@ -7,9 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IziTableComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    console.log("Method not implemented")
   }
 
+  @Input() listaAgendamentos: Agendamento[] = [];
+  @Input() listaClientes: Cliente[] = [];
+  @Input() listaEstabelecimentos: Estabelecimento[] = [];
+  @Input() listaProdutos: Produto[] = [];
+  @Input() listaReservas: Reserva[] = [];
+  @Input() listaServicos: Servico[] = [];
+
+  colunas: string[] = ['']
 }
