93ddae2e7074bbaf5ed432ac22882a6511bfadcc
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-09-29 18:56:58 -0300

    modais abrindo pós login, esperando alteração do back para fazer o metodo if

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import {
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component
 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,67 +12,86 @@ import icVisibilityOff from '@iconify/icons-ic/twotone-visibility-off';
 import { fadeInUp400ms } from '../../../../@vex/animations/fade-in-up.animation';
 import { Usuario } from 'src/app/core/model/usuario';
 import { EstabelecimentoService } from 'src/app/core/services/estabelecimento/estabelecimento.service';
+import { LoginModalComponent } from 'src/app/core/lib/components/modais/primeiro-login/login-modal/login-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { PlanosComponent } from 'src/app/core/lib/components/modais/planos/planos-modal/planos.component';
 
 @Component({
-    selector: 'vex-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [fadeInUp400ms]
+  selector: 'vex-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeInUp400ms]
 })
 export class LoginComponent {
-    form: FormGroup;
+  form: FormGroup;
 
-    inputType = 'password';
-    visible = false;
+  inputType = 'password';
+  visible = false;
 
-    icVisibility = icVisibility;
-    icVisibilityOff = icVisibilityOff;
+  icVisibility = icVisibility;
+  icVisibilityOff = icVisibilityOff;
 
-    public usuario = new Usuario();
+  public usuario = new Usuario();
 
-    constructor(
-        private router: Router,
-        private fb: FormBuilder,
-        private cd: ChangeDetectorRef,
-        private snackbar: MatSnackBar,
-        private estabelecimentoService: EstabelecimentoService
-    ) {
-        this.form = this.fb.group({
-            email: ['', Validators.required],
-            password: ['', Validators.required]
-        });
-    }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private cd: ChangeDetectorRef,
+    private snackbar: MatSnackBar,
+    private estabelecimentoService: EstabelecimentoService,
+    public dialog: MatDialog
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
-    login() {
-        this.estabelecimentoService.saveUsuario(this.usuario).subscribe(response =>
-        {
-          localStorage.setItem("login", JSON.stringify(response));
-          this.router.navigate(['/']);
-          console.log(localStorage.getItem("login"));
-        },
-          (error) => {
+  login() {
+    this.estabelecimentoService.saveUsuario(this.usuario).subscribe(response => {
+      localStorage.setItem("login", JSON.stringify(response));
+      this.router.navigate(['/']);
+      this.openModalCadastro();
+    },
+      (error) => {
         this.snackbar.open
-        (
+          (
             'Email ou senha incorretos, ou usuario nao cadastrado',
             'fechar',
             {
-                duration: 5000
+              duration: 5000
             }
-        );
-            console.log(error)
-          });
-    }
+          );
+        console.log(error)
+      });
+  }
 
-    toggleVisibility() {
-        if (this.visible) {
-            this.inputType = 'password';
-            this.visible = false;
-            this.cd.markForCheck();
-        } else {
-            this.inputType = 'text';
-            this.visible = true;
-            this.cd.markForCheck();
-        }
+  toggleVisibility() {
+    if (this.visible) {
+      this.inputType = 'password';
+      this.visible = false;
+      this.cd.markForCheck();
+    } else {
+      this.inputType = 'text';
+      this.visible = true;
+      this.cd.markForCheck();
     }
+  }
+
+  openModalCadastro() {
+
+    const dialogRef = this.dialog.open(LoginModalComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.dialog.open(PlanosComponent);
+    });
+
+  }
+
+  abrirModais() {
+    
+  }
 }
+
+
