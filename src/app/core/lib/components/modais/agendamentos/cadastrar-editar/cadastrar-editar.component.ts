efc621a898a15df474b8d22c6d2b22454874aed8
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-09-29 22:09:39 -0300

    padronizando o modal de adicionar agendamento

--- src/app/core/lib/components/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
+++ src/app/core/lib/components/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Agendamento } from 'src/app/core/model/agendamento';
 
@@ -9,28 +9,28 @@ import { Agendamento } from 'src/app/core/model/agendamento';
   styleUrls: ['./cadastrar-editar.component.scss']
 })
 export class CadastrarEditarComponent {
-  public clientForm: FormGroup;
+  form: FormGroup;
 
   public userData?: Agendamento;
 
   constructor(
     private readonly fb: FormBuilder,
-    private readonly dialogRef: MatDialogRef<any>,
+    private readonly dialogRef: MatDialogRef<CadastrarEditarComponent>,
   ) {
-    this.clientForm = fb.group({
-      cliente: [''],
-      servico: [''],
-      tempo: [''],
-      valor: [''],
-      data: [''],
-      responsavel: [''],
-      status: [''],
-    })
+    this.form = this.fb.group({
+      cliente: ['', Validators.required],
+      servico: ['', Validators.required],
+      tempo: ['', Validators.required],
+      valor: ['', Validators.required],
+      data: ['', Validators.required],
+      status: ['', Validators.required],
+      hora: ['', Validators.required]
+  })
 
   }
 
   public addUser() {
-    this.userData = this.clientForm.value;
+    this.userData = this.form.value;
     this.dialogRef.close(this.userData);
   }
 
