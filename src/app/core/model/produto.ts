43993287ecf70b37de007127c701d302ad87ec70
brunodeveloperbest <129685774+brunodeveloperbest@users.noreply.github.com>
2023-10-03 19:35:53 -0300

    modificação de export interface no model produtos

--- src/app/core/model/produto.ts
+++ src/app/core/model/produto.ts
@@ -1,12 +1,12 @@
 export interface Produto{
-    [x: string]: any;
-    codProduto: null,
-    nomeProduto: string,
-    dsProduto: string,
-    qtdEstoque: number,
-    valor: number,
-    valorPromocional: number,
-    ativo: true,
-    promocional: false,
-    estabelecimentoID: number
+    id: number;
+    codigo: null;
+    nome: string;
+    descricao: string;
+    qtdEstoque: number;
+    valor: number;
+    valorPromocional: number;
+    ativo: true;
+    promocional: false;
+    estabelecimentoID: number;
 }
