7e2d42cd7cf09e540c507ac2a5735d69def05018
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-10-05 16:13:17 -0300

    ajustando função para editar o produto

--- src/app/core/lib/components/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
+++ src/app/core/lib/components/modais/agendamentos/cadastrar-editar/cadastrar-editar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Agendamento } from 'src/app/core/model/agendamento';
+import { AgendamentoService } from 'src/app/core/services/agendamentos/agendamentos.service';
+import { ExibicaoAgendamentosComponent } from 'src/app/views/pages/agendamentos/exibicao-agendamentos/exibicao-agendamentos.component';
 
 @Component({
   selector: 'vex-cadastrar-editar',
@@ -9,6 +11,7 @@ import { Agendamento } from 'src/app/core/model/agendamento';
   styleUrls: ['./cadastrar-editar.component.scss']
 })
 export class CadastrarEditarComponent {
+
   public clientForm: FormGroup;
 
   public userData?: Agendamento;
@@ -16,6 +19,8 @@ export class CadastrarEditarComponent {
   constructor(
     private readonly fb: FormBuilder,
     private readonly dialogRef: MatDialogRef<any>,
+    private agendamentoService: AgendamentoService,
+    private exibicaoAgendamento: ExibicaoAgendamentosComponent
   ) {
     this.clientForm = fb.group({
       cliente: [''],
@@ -29,9 +34,14 @@ export class CadastrarEditarComponent {
 
   }
 
-  public addUser() {
-    this.userData = this.clientForm.value;
-    this.dialogRef.close(this.userData);
+  cadastrarAgendamento(agendamento: Agendamento) {
+    this.agendamentoService.CadastraAgendamentos(this.clientForm.value).subscribe(response => {
+      this.exibicaoAgendamento.getAgendamentos();
+
+    }, (error) => {
+
+    })
+    
   }
 
 }
