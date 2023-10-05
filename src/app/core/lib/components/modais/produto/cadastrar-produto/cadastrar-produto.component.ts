e4c822765eef250eed21ace0a3699df4b0c5f3cb
Guilherme Pedro <guilherme_pedro12@hotmail.com>
2023-10-05 20:36:18 -0300

    trabalhando na lógica para chamar a função editar

--- src/app/core/lib/components/modais/produto/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/core/lib/components/modais/produto/cadastrar-produto/cadastrar-produto.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ProdutoService } from 'src/app/core/services/produtos/produtos.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { Produto } from 'src/app/core/model/produto';
+
 
 @Component({
   selector: 'vex-cadastrar-produto',
@@ -12,40 +15,85 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class CadastrarProdutoComponent {
 
   form: FormGroup;
+  legendaBotao: string = '';
+  isCadastro!: boolean;
 
-    constructor(private fb: FormBuilder, private produtoService: ProdutoService, 
-      private readonly dialogRef: MatDialogRef<CadastrarProdutoComponent>, 
-      private snackbar: MatSnackBar, @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
-      this.form = this.fb.group({
-        nome: [data?.produto?.nomeProduto, Validators.required],
-        quantidade: [data?.produto?.qtdEstoque, Validators.required],
-        valor: [data?.produto?.valor, Validators.required],
-        categoria: ['', Validators.required]
-      });
-    }
+  servico = new Produto();
 
-    cadastrarProduto() {
-      this.produtoService.cadastrarProduto(this.form.value).subscribe(() => {
-        console.log(this.form.value);
-        this.dialogRef.close();
+  constructor(private fb: FormBuilder, private produtoService: ProdutoService,
+    private readonly dialogRef: MatDialogRef<CadastrarProdutoComponent>,
+    private snackbar: MatSnackBar, @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.isCadastro = data.servico ? false : true;
+    this.legendaBotao = this.isCadastro ? "Adicionar" : "Confirmar";
+    this.form = this.fb.group({
+      nome: [data?.produto?.nomeProduto, Validators.required],
+      quantidade: [data?.produto?.qtdEstoque, Validators.required],
+      valor: [data?.produto?.valor, Validators.required],
+      categoria: ['', Validators.required]
+    });
+  }
+
+  cadastrarEditarProduto() {
+    this.isCadastro?this.cadastrarProduto() : this.editarProduto()
+  }
+
+
+  cadastrarProduto() {
+    this.produtoService.cadastrarProduto(this.form.value).subscribe(() => {
+      console.log(this.form.value);
+      this.dialogRef.close();
+      this.snackbar.open(
+        'Cadastro de produto realizado com sucesso!',
+        'FECHAR',
+        {
+          duration: 10000
+        }
+      );
+    },
+      (error) => {
+        console.log(error)
         this.snackbar.open(
-          'Cadastro de produto realizado com sucesso!',
+          'Produto não cadastrado.',
           'FECHAR',
           {
-              duration: 10000
+            duration: 10000
           }
-      );
-      },
-        (error) => {
-          console.log(error)
-          this.snackbar.open(
-            'Produto não cadastrado.',
-            'FECHAR',
+        );
+      })
+  }
+
+  editarProduto(): void {
+    this.montarBody();
+    this.produtoService.editaProduto(this.produto).subscribe(response => {
+        console.log(this.form.value);
+        this.dialogRef.close(true);
+        this.snackbar.open(
+            "Produto editado com sucesso",
+            "Fechar",
+            {
+              duration: 10000
+            }
+          );
+
+    }, (error) => {
+        console.log(error)
+        this.snackbar.open(
+            "Produto não editado",
+            "Tenta novamente",
             {
                 duration: 10000
             }
         );
-        })
-    }
+    })
+}
+
+private montarBody() {
+  let id = this.data?.produto?.id;
+  let estabelecimentoID = this.data?.produto?.estabelecimentoID;
+  this.produto = this.form.value;
+  this.produto.id = id;
+  this.produto.estabelecimentoID = estabelecimentoID;
+}
 
 }
