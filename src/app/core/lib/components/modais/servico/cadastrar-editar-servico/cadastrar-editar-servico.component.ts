ae03b7203de85eb53a96b061e74d6ca74622672f
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-10-06 04:45:12 -0300

    todos os loads adicionados, um tipo para as tabelas e um outro para as telas e modais pós click

--- src/app/core/lib/components/modais/servico/cadastrar-editar-servico/cadastrar-editar-servico.component.ts
+++ src/app/core/lib/components/modais/servico/cadastrar-editar-servico/cadastrar-editar-servico.component.ts
@@ -7,48 +7,54 @@ import { ServicoService } from 'src/app/core/services/servico/servico.service';
 
 
 @Component({
-    selector: 'vex-cadastrar-editar-servico',
-    templateUrl: './cadastrar-editar-servico.component.html',
-    styleUrls: ['./cadastrar-editar-servico.component.scss']
+  selector: 'vex-cadastrar-editar-servico',
+  templateUrl: './cadastrar-editar-servico.component.html',
+  styleUrls: ['./cadastrar-editar-servico.component.scss']
 })
 export class CadastrarEditarServicoComponent {
-    form: FormGroup;
 
-    constructor(private fb: FormBuilder, private servicoService: ServicoService,
-        private readonly dialogRef: MatDialogRef<CadastrarEditarServicoComponent>,
-        private snackbar: MatSnackBar) {
-        this.form = this.fb.group({
-            nome: ['', Validators.required],
-            tempo: ['', Validators.required],
-            categoria: ['', Validators.required],
-            valor: ['', Validators.required]
-        });
-    }
+  load: boolean = false;
 
-    cadastrarServico() {
-        this.servicoService.cadastrarServico(this.form.value).subscribe(() => {
-            console.log(this.form.value);
-            this.dialogRef.close()
-            this.snackbar.open(
-                MENSAGENS.ADICIONAR_SERVICO,
-                "Fechar",
-                {
-                    duration: 10000
-                }
-            );
-        },
-            (error) => {
-                console.log(error);
-                this.snackbar.open(
-                    MENSAGENS.ERRO_ADICIONAR_SERVICO,
-                    "Tente novamente",
-                    {
-                        duration: 10000
-                    }
-                );
+  form: FormGroup;
 
-            })
-    }
+  constructor(private fb: FormBuilder, private servicoService: ServicoService,
+    private readonly dialogRef: MatDialogRef<CadastrarEditarServicoComponent>,
+    private snackbar: MatSnackBar) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      tempo: ['', Validators.required],
+      categoria: ['', Validators.required],
+      valor: ['', Validators.required]
+    });
+  }
+
+  cadastrarServico() {
+    this.load = true;
+    this.servicoService.cadastrarServico(this.form.value).subscribe(() => {
+      console.log(this.form.value);
+      this.load = false;
+      this.dialogRef.close()
+      this.snackbar.open(
+        MENSAGENS.ADICIONAR_SERVICO,
+        "Fechar",
+        {
+          duration: 10000
+        }
+      );
+    },
+      (error) => {
+        this.load = false;
+        console.log(error);
+        this.snackbar.open(
+          MENSAGENS.ERRO_ADICIONAR_SERVICO,
+          "Tente novamente",
+          {
+            duration: 10000
+          }
+        );
+
+      })
+  }
 
 
 
