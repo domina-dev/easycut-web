0b618f511f27d0099b5ad40a935844485642bd88
Victor <vitorsntossouza9866@gmail.com>
2023-10-08 11:40:13 -0300

    os agendamentos do dia leva para tela de agendamentos e coloca o agendamento selecionado em destaque

--- src/@vex/layout/quickpanel/quickpanel.component.ts
+++ src/@vex/layout/quickpanel/quickpanel.component.ts
@@ -1,40 +1,44 @@
+import { Router } from '@angular/router';
 import { AgendamentoService } from './../../../app/core/services/agendamentos/agendamentos.service';
 import { Component, OnInit } from '@angular/core';
 import { DateTime } from 'luxon';
 import { Agendamento } from 'src/app/core/model/agendamento';
 import { EventEmitterService } from 'src/app/core/services/event.service';
+import { LayoutService } from 'src/@vex/services/layout.service';
 
 
 @Component({
-    selector: 'vex-quickpanel',
-    templateUrl: './quickpanel.component.html',
-    styleUrls: ['./quickpanel.component.scss']
+  selector: 'vex-quickpanel',
+  templateUrl: './quickpanel.component.html',
+  styleUrls: ['./quickpanel.component.scss']
 })
-export class QuickpanelComponent implements OnInit{
-    date = DateTime.local().toFormat('DD');
-    dayName = DateTime.local().toFormat('EEEE');
+export class QuickpanelComponent implements OnInit {
+  date = DateTime.local().toFormat('DD');
+  dayName = DateTime.local().toFormat('EEEE');
 
-    listaAgendamentos: Agendamento[] = [];
-    agendamento: Agendamento[] = []
+  listaAgendamentos: Agendamento[] = [];
+  agendamento: Agendamento[] = []
 
-    constructor (private agendamentoService: AgendamentoService) { }
+  constructor(private agendamentoService: AgendamentoService,
+    private router: Router,
+    private layoutService: LayoutService) { }
 
-    ngOnInit(): void {
-      EventEmitterService.get("buscarAgendamentosDoDia").subscribe(()=> this.getAgendamentosDia())
-    }
+  ngOnInit(): void {
+    EventEmitterService.get("buscarAgendamentosDoDia").subscribe(() => this.getAgendamentosDia())
+  }
 
-    getAgendamentosDia() {
-      this.agendamentoService.getAgendamentosDia().subscribe(response => {
-        this.listaAgendamentos = response;
-        console.log(response)
-      },
-        (error) => { console.log(error)});
-    }
+  getAgendamentosDia() {
+    this.agendamentoService.getAgendamentosDia().subscribe(response => {
+      this.listaAgendamentos = response;
+      console.log(response)
+    },
+      (error) => { console.log(error) });
+  }
 
-    mostraAgendamento(): void {
-      this.agendamentoService.getAgendamentos().subscribe(respose => {
-        this.agendamento = respose;
-      });
-      console.log(this.agendamento)
-    }
+  mostraAgendamento(agendamentoHoje: Agendamento): void {
+    window.localStorage.setItem('agendamentoHoje', agendamentoHoje.id.toString());
+    this.router.navigate(['/agendamentos']);
+    this.layoutService.closeQuickpanel();
+    EventEmitterService.get("buscarAgendamentos").emit();
+  }
 }
