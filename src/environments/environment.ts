b9232756832aceb29a81f35c791391f5df13d467
Vitor Macedo <evitorfiler@gmail.com>
2023-10-11 16:56:25 -0300

    mudando backend para AWS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    url_api: 'https://easycut-dev.herokuapp.com',
+    url_api: 'http://ec2-18-230-138-242.sa-east-1.compute.amazonaws.com:8080',
     url_vitor: 'http://localhost:3000'
 };
