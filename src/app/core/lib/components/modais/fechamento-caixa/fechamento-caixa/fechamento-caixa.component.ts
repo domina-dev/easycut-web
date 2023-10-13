e9dbde10b204edd975357493a2d1ca589b45c213
Victor <vitorsntossouza9866@gmail.com>
2023-10-13 12:30:57 -0300

    criei os components para o modal e fiz a funcao que abre o modal quando clicar em fechar caixa

---
+++ src/app/core/lib/components/modais/fechamento-caixa/fechamento-caixa/fechamento-caixa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vex-fechamento-caixa',
+  templateUrl: './fechamento-caixa.component.html',
+  styleUrls: ['./fechamento-caixa.component.scss']
+})
+export class FechamentoCaixaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
