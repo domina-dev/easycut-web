c7bb2f0b12cfa8187f35902d55f34e0f8b7ec363
Geanderson74 <geandersonrodrigues755@gmail.com>
2023-10-15 22:15:11 -0300

    criação do calendário de agendamento

--- src/app/views/pages/agenda/agenda.component.ts
+++ src/app/views/pages/agenda/agenda.component.ts
@@ -1,8 +1,172 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
+import { addDays, addHours, endOfDay, endOfMonth, isSameDay, isSameMonth, startOfDay, subDays } from 'date-fns';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+// import { CalendarEditComponent } from './calendar-edit/calendar-edit.component';
+import icChevronLeft from '@iconify/icons-ic/twotone-chevron-left';
+import icChevronRight from '@iconify/icons-ic/twotone-chevron-right';
+
+const colors: any = {
+    blue: {
+      primary: '#5c77ff',
+      secondary: '#FFFFFF'
+    },
+    yellow: {
+      primary: '#ffc107',
+      secondary: '#FDF1BA'
+    },
+    red: {
+      primary: '#f44336',
+      secondary: '#FFFFFF'
+    }
+  };
 
 @Component({
     selector: 'vex-agenda',
     templateUrl: './agenda.component.html',
-    styleUrls: ['./agenda.component.scss']
+    styleUrls: ['./agenda.component.scss'],
 })
-export class AgendaComponent {}
+export class AgendaComponent {
+    @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+  icChevronLeft = icChevronLeft;
+  icChevronRight = icChevronRight;
+
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  };
+  refresh: Subject<any> = new Subject();
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fa fa-fw fa-pencil"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      }
+    },
+    {
+      label: '<i class="fa fa-fw fa-times"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      }
+    }
+  ];
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.primary,
+      actions: this.actions,
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+      actions: this.actions
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.primary,
+      allDay: true
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: new Date(),
+      title: 'A draggable and resizable event',
+      color: colors.red,
+      actions: this.actions,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    }
+  ];
+  activeDayIsOpen = true;
+
+  constructor(private dialog: MatDialog,
+              private snackbar: MatSnackBar) {}
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      this.activeDayIsOpen = !((isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) || events.length === 0);
+      this.viewDate = date;
+    }
+  }
+
+  eventTimesChanged({ event, newStart, newEnd }: CalendarEventTimesChangedEvent): void {
+    this.events = this.events.map(iEvent => {
+      if (iEvent === event) {
+        return {
+          ...event,
+          start: newStart,
+          end: newEnd
+        };
+      }
+      return iEvent;
+    });
+    this.handleEvent('Dropped or resized', event);
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    console.log(event);
+
+    // const dialogRef = this.dialog.open(CalendarEditComponent, {
+    //   data: event
+    // });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if (result) {
+    //     event = result;
+    //     this.snackbar.open('Updated Event: ' + event.title);
+    //     this.refresh.next();
+    //   }
+    // });
+  }
+
+  addEvent(): void {
+    this.events = [
+      ...this.events,
+      {
+        title: 'New event',
+        start: startOfDay(new Date()),
+        end: endOfDay(new Date()),
+        color: colors.red,
+        draggable: true,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true
+        }
+      }
+    ];
+  }
+
+  deleteEvent(eventToDelete: CalendarEvent) {
+    this.events = this.events.filter(event => event !== eventToDelete);
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+}
