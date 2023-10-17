2ce6b0cbd68783f8dd42ae89bef4dae23b1ff6fa
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-10-17 10:20:25 -0300

    grade de itens selecionados

--- src/app/views/pages/caixa/nova-venda/nova-venda.component.ts
+++ src/app/views/pages/caixa/nova-venda/nova-venda.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
 @Component({
     selector: 'vex-nova-venda',
@@ -12,6 +18,16 @@ export class NovaVendaComponent implements OnInit {
   servicos: boolean;
 
 
+
+  displayedColumns: string[] = [
+    'selecionado',
+    'quantidade',
+    'valor',
+];
+
+dataSource = ELEMENT_DATA;
+
+
   if (_favoriteSeason = 'Serviços') {
 this.servicos = true
   };
@@ -20,3 +36,17 @@ this.servicos = true
         console.log('Method not implemented');
     }
 }
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
