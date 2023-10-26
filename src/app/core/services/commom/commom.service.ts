9bbc6a76338728ad07232c96e405cf7d137dc2d7
Vitor Macedo <evitorfiler@gmail.com>
2023-10-26 12:47:11 -0300

    busca de estabelecimento logado após logar

--- src/app/core/services/commom/commom.service.ts
+++ src/app/core/services/commom/commom.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Estabelecimento } from '../../model/estabelecimento';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CommomService {
+    
     estabelecimentoId: number = +window.localStorage.getItem("estabelecimento_ID");
-    constructor(private http: HttpClient) {}
+    estabelecimento = new Estabelecimento();
+    
+    constructor(public http: HttpClient) {}
 
     healthCheck(): Observable<any> {
         return this.http
