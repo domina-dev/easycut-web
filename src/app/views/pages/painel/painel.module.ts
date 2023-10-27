7570e518a1aac5c3126f6cd07b6ed7757f797a81
Geanderson74 <geandersonrodrigues755@gmail.com>
2023-10-27 19:47:49 -0300

    Criação do Page layout no Painel

--- src/app/views/pages/painel/painel.module.ts
+++ src/app/views/pages/painel/painel.module.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { PainelRoutingModule } from './painel-routing.module';
 import { PainelComponent } from './painel.component';
 import { ConstrucaoModalModule } from 'src/app/core/lib/components/modais/construcao-modal/construcao-modal.module';
+import { PageLayoutModule } from "../../../../@vex/components/page-layout/page-layout.module";
 
 
 @NgModule({
-  declarations: [
-    PainelComponent
-  ],
-  imports: [
-    CommonModule,
-    PainelRoutingModule,ConstrucaoModalModule 
-  ]
+    declarations: [
+        PainelComponent
+    ],
+    imports: [
+        CommonModule,
+        PainelRoutingModule, ConstrucaoModalModule,
+        PageLayoutModule
+    ]
 })
 export class PainelModule { }
