236a3771682b55c619a3c939f5616234988df259
Joao Pedro FiuZA <129235504+Jotapey33@users.noreply.github.com>
2023-11-14 20:07:54 -0300

    modal criada, criada pasta para exibição dos produtos em lista

---
+++ src/app/views/pages/servicos/exibicao-produtos/lista-produtos/lista-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vex-lista-produtos',
+  templateUrl: './lista-produtos.component.html',
+  styleUrls: ['./lista-produtos.component.scss']
+})
+export class ListaProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/views/pages/servicos/exibicao-produtos/lista-produtos/lista-produtos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaProdutosComponent } from './lista-produtos.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListaProdutosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListaProdutosModule { }
