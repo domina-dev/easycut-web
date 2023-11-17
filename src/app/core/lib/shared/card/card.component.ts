cfaff69065869c36e1c0fd3e7c5d7cdb4226070b
Victor <vitorsntossouza9866@gmail.com>
2023-11-17 17:55:47 -0300

    mudando local do mat menu no card

--- src/app/core/lib/shared/card/card.component.ts
+++ src/app/core/lib/shared/card/card.component.ts
@@ -42,7 +42,6 @@ export class CardComponent implements OnInit {
   }
 
   abrirModalPromocional() {
-    this.eventModalPromocional.emit({ servico: this.item })
+    this.eventModalPromocional.emit({ servico: this.item})
   }
-
 }
